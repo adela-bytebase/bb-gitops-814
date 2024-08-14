37298ab57afa5f18252b78b70c202864d0dc8498
Adela <a@bytebase.com>
2024-08-14 16:04:18 +0800

    Update 20248141530_create_t2.sql

--- bytebase/20248141530_create_t2.sql
+++ bytebase/20248141530_create_t2.sql
@@ -1,4 +1,4 @@
    CREATE TABLE "public"."t2" (
-      "id" integer NOT NULL PRIMARY KEY,
-      "name" text NULL
+      "id" integer NOT NULL DEFAULT 0 PRIMARY KEY,
+      "name" text NOT NULL DEFAULT ''
    );
